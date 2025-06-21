b28f3a16746c74be3c04416ba3f10566a1204596
Popov Stas <boss.staspopov@mail.ru>
2025-06-21 16:39:40 +0400

    change doc-string in setup_logging

--- app/core/logs.py
+++ app/core/logs.py
@@ -4,7 +4,7 @@ import logging
 
 
 def setup_logging() -> None:
-    """Загружает конфигурацию логирования из YAML-файла."""
+    """Load logging configuration from YAML-file."""
     with open('config/logging.yaml') as f:
         config = yaml.safe_load(f)
         logging.config.dictConfig(config)
